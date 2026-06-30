4528fa6d731ec52f2a68e64e0254fc470cb56091
dejan <dejanr77@yahoo.com>
2026-06-30 08:04:48 +0200

    min setup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::shouldBeStrict(shouldBeStrict: true);
+
+        DB::prohibitDestructiveCommands(
+            prohibit: config(key: 'app.env') === 'production'
+        );
     }
 }
